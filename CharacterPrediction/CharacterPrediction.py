e7b33c62a57e6a1358500871414382dcd04736c3
Maxwell Carlson <bart4282819@gmail.com>
2018-08-23 20:23:03 -0700

    Trying to get the basic version of this LSTM text predictor up and running. Added some functions for splitting data into to train, test , and validation sets

--- CharacterPrediction/CharacterPrediction.py
+++ CharacterPrediction/CharacterPrediction.py
@@ -13,6 +13,20 @@ outputSize  = 1
 lr          = 0.02
 batchSize   = 64
 maxEpochs   = 100
+numClasses  = 255
+
+
+def splitData(data, valSize = 0.1, testSize = 0.1):
+
+    length      = len(data)
+    trainLen    = int((1.0 - valSize - testSize) * length)
+    valLen      = int(valSize  * length)
+    testLen     = int(testSize * length)
+
+
+    train, val, test = data[0:trainLen], data[trainLen:trainLen+valLen], data[trainLen+valLen:]
+
+    return {"train": train, "val": val, "test": test}
 
 def loadData(path, timeSteps, timeShift):
     
@@ -21,7 +35,7 @@ def loadData(path, timeSteps, timeShift):
     file.close()
 
     # Pair lines down more for expedient testing early on
-    lines = lines[0:10000]
+    lines = lines[0:1000]
     
     # Convert the character data into numbers
     data = []
@@ -42,7 +56,7 @@ def loadData(path, timeSteps, timeShift):
 
     Y = np.array(data[timeShift + timeSteps - 1 :])
 
-    return X, Y
+    return X, Y#splitData(X), splitData(Y)
 
 def createNetwork(input):
 
@@ -50,23 +64,43 @@ def createNetwork(input):
         n = cntk.layers.Recurrence(cntk.layers.LSTM(timeSteps))(input)
         n = cntk.sequence.last(n)
         n = cntk.layers.Dropout(0.4)(n)
-        n = cntk.layers.Dense(outSize)(n)
+        n = cntk.layers.Dense(numClasses)(n)
         return n
 
-def genBatch(X, Y):
-    pass
+def genBatch(X, Y, dataSet):
+    # TODO: Need to look into randomizing the data's order
 
+    def asBatch(data, start, count):
+        part = []
+        for i in range(start, start+count):
+            part.append(data[i])
+        return np.array(part)
+
+    for i in range(0, len(X) - batchSize, batchSize):
+        yield asBatch(X, i, batchSize), asBatch(Y, i, batchSize)
+
+    #for i in range(0, len(X[dataSet]) - batchSize, batchSize):
+    #    yield asBatch(X[dataSet], i, batchSize), asBatch(Y[dataSet], i, batchSize)
+
+# TODO: Look into Attention based models 
+# i.e. something like cntk.layers.attention
+# https://arxiv.org/pdf/1502.03044.pdf
+# https://towardsdatascience.com/memory-attention-sequences-37456d271992
+#
 def trainNetwork():
-
+    
     #xAxes = [cntk.Axis.default_batch_axis(), cntk.Axis.default_dynamic_axis()]
     #input = cntk.input_variable(1, dynamic_axes=xAxes)
-    input   = cntk.sequence.input_variable(1)
+    input   = cntk.sequence.input_variable(numClasses)
 
     X, Y    = loadData(dir + fileName, timeSteps, timeShift)
 
+    X = cntk.one_hot(X, numClasses)
+    Y = cntk.one_hot(Y, numClasses)
+
     model   = createNetwork(input)
 
-    label   = cntk.input_variable(1, dynamic_axes=model.dynamic_axes, name='label')
+    label   = cntk.input_variable(numClasses, dynamic_axes=model.dynamic_axes, name='label')
 
     loss    = cntk.cross_entropy_with_softmax(model, label) #cntk.squared_error(model, label)
     error   = cntk.cross_entropy_with_softmax(model, label) #cntk.squared_error(model, label)
@@ -77,8 +111,9 @@ def trainNetwork():
 
 
     for epoch in range(maxEpochs):
-        for X1, Y1 in genBatch():
-            pass
+        for X1, Y1 in genBatch(X, Y, "train"):
+            trainer.train_minibatch({X: X1, Y: Y1})
+        print("epoch: {}, loss: {:.3f}".format(epoch, trainer.previous_minibatch_loss_average))
 
     
 
